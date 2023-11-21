5874588690054fa974ae55f73b1b04de3004b376
George <georgejoj28@gmail.com>
2023-11-21 21:44:37 +0300

    change projects page and links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,36 +1,50 @@
-import macbk from '../assets/macbk.jpeg';
-import graffiti from '../assets/graffiti.png';
+import crop from '../assets/crop.png';
 import rainbow from '../assets/rainbow.png';
+import crop2 from '../assets/crop2.png';
 
 
 const Projects = () => {
     return ( 
-        <div>
+        <div className='project-content'>
             <div>
                 <h1 className='text-primary-emphasis p-lg-5 text-center'>MY RECENT PROJECTS</h1>
             </div>
             <div className='container row project'>
-                <div className="card col-md-3" >
-                    <img src={macbk} className="card-img-top img-fluid pt-2" alt="..." />
+                <div className="card col-md-6" >
+                    <img src={crop} className="card-img-top img-fluid pt-2" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Netfluxx Movie Website</h5>
-                        <p className="card-text">A livestreaming movie application that has a variety of movie genres to choose from.</p>
-                        <a href="https://github.com/ohthebrave/Netfluxx-Clone.git" className="btn btn-outline-secondary">View Project</a>
+                        <h5 className="card-title text-success font-monospace">Netfluxx Movie Website</h5>
+                        <p className="card-text fw-lighter text-white">A livestreaming movie application that has a variety of movie genres to choose from.</p>
+                        <a href="https://gbox-movie.netlify.app/" className="btn btn-outline-secondary">View Project</a>
                     </div>
                 </div>
 
-                <div className="card col-md-3">
-                    <img src={graffiti} className="card-img-top img-fluid pt-2" alt="..." />
+                <div className="card col-md-6">
+                    <img src={crop2} className="card-img-top img-fluid pt-2" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">CSS Graffiti</h5>
-                        <p className="card-text">Imagine that you are walking down the street, and you notice that there's a mural that has been painted 
-                        over with various graffiti tags. In reality, cleaning this up could be a challenging feat; however, in CSS, you have the power to 
-                        manipulate the DOM with just a line or two of code and to restore the mural to its original look!</p>
-                        <a href="https://github.com/ohthebrave/phase-0-css-graffiti-lab" className="btn btn-outline-secondary">View Project</a>
+                        <h5 className="card-title text-success font-monospace">Weather-App</h5>
+                        <p className="card-text fw-lighter text-white">A simple weather application that is user-friendly and users can be able to search current weather data .</p>
+                        <a href="https://ohthebrave.github.io/weather-app/" className="btn btn-outline-secondary">View Project</a>
                     </div>
                 </div>
 
-                <div className="card col-md-3">
+                <div className="card col-md-6">
+                    <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title text-success font-monospace">My Little rainbow</h5>
+                        <p className="card-text fw-lighter text-white">Making a rainbow with HTML div elements using HTML elements, CSS styling, and CSS selectors.</p>
+                        <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
+                    </div>
+                </div>
+                <div className="card col-md-6">
+                    <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title text-success font-monospace">My Little rainbow</h5>
+                        <p className="card-text fw-lighter text-white">Making a rainbow with HTML div elements using HTML elements, CSS styling, and CSS selectors.</p>
+                        <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
+                    </div>
+                </div>
+                {/* <div className="card col-md-6">
                     <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">My Little rainbow</h5>
@@ -38,7 +52,7 @@ const Projects = () => {
                         <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
                     </div>
                 </div>
-                <div className="card col-md-3">
+                <div className="card col-md-6">
                     <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">My Little rainbow</h5>
@@ -46,7 +60,7 @@ const Projects = () => {
                         <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
                     </div>
                 </div>
-                <div className="card col-md-3">
+                <div className="card col-md-6">
                     <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">My Little rainbow</h5>
@@ -54,7 +68,7 @@ const Projects = () => {
                         <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
                     </div>
                 </div>
-                <div className="card col-md-3">
+                <div className="card col-md-6">
                     <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">My Little rainbow</h5>
@@ -62,30 +76,14 @@ const Projects = () => {
                         <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
                     </div>
                 </div>
-                <div className="card col-md-3">
+                <div className="card col-md-6">
                     <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">My Little rainbow</h5>
                         <p className="card-text">Making a rainbow with HTML div elements using HTML elements, CSS styling, and CSS selectors.</p>
-                        <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
-                    </div>
-                </div>
-                <div className="card col-md-3">
-                    <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">My Little rainbow</h5>
-                        <p className="card-text">Making a rainbow with HTML div elements using HTML elements, CSS styling, and CSS selectors.</p>
-                        <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
-                    </div>
-                </div>
-                <div className="card col-md-3">
-                    <img src={rainbow} className="card-img-top img-fluid pt-2" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">My Little rainbow</h5>
-                        <p className="card-text">Making a rainbow with HTML div elements using HTML elements, CSS styling, and CSS selectors.</p>
-                        <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a>
-                    </div>
-                </div>
+                        <a href="https://github.com/ohthebrave/phase-0-css-rainbow-lab" className="btn btn-outline-secondary">View Project</a> */}
+                    {/* </div> */}
+                {/* </div> */}
             </div>
         </div>
      );
